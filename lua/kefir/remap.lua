bf8490541ebf7d7ac4a70a6ba324f1635d609987
Sergey Tkachenko <sergey.iray@yandex.ru>
2023-07-31 16:26:24 +0300

    Added: Command mappings for Russian language

--- lua/kefir/remap.lua
+++ lua/kefir/remap.lua
@@ -16,3 +16,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>z", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Russian language remaps
+vim.keymap.set("n", "Ж", ":")
